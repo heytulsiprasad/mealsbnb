e41f1af0c9aa12f92d4ce225759e2cd00938af02
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-05-22 22:27:26 +0530

    Add flatlist to restaurants screen

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -9,12 +9,10 @@ export const Title = styled.Text`
 
 export const RestaurantCard = styled(Card)`
   background-color: white;
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
-export const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.brand.secondary};
-  padding: 20px;
-`;
+export const RestaurantCardCover = styled(Card.Cover)``;
 
 export const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
@@ -15,21 +15,34 @@ const SearchBarView = styled.View`
   padding: 16px;
 `;
 
-const ListBarView = styled.View`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  padding: 16px;
-  flex: 1;
-`;
-
 const RestaurantsScreen = () => {
   return (
     <StyledSafeAreaView>
       <SearchBarView>
         <Searchbar placeholder="Search" />
       </SearchBarView>
-      <ListBarView>
-        <RestaurantInfoCard />
-      </ListBarView>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name}
+        // eslint-disable-next-line react-native/no-inline-styles
+        contentContainerStyle={{ padding: 16 }}
+      />
     </StyledSafeAreaView>
   );
 };
