033c0233b668f1dfb2c7b6d5ee41d568d7210a61
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-05-23 08:25:03 +0530

    Add bottom tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { View, Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { SafeArea } from "./src/components/utility/safe-area.component";
 
 import {
   useFonts as useOswald,
@@ -14,6 +20,28 @@ import {
 
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
+
+const SettingsScreen = () => {
+  return (
+    <SafeArea
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>This is Settings!</Text>
+    </SafeArea>
+  );
+};
+
+const MapScreen = () => {
+  return (
+    <SafeArea
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Text>This is Maps!</Text>
+    </SafeArea>
+  );
+};
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   const [oswaldLoaded] = useOswald({
@@ -30,12 +58,40 @@ const App = () => {
   }
 
   return (
-    <>
+    <NavigationContainer>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              switch (route.name) {
+                case "Restaurant":
+                  iconName = "md-restaurant";
+                  break;
+                case "Settings":
+                  iconName = "md-settings";
+                  break;
+                case "Map":
+                  iconName = "md-map";
+                  break;
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "orange",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
-    </>
+    </NavigationContainer>
   );
 };
 
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -21,7 +21,7 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Restaurant New Orleans",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-    photos = "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    photos = "https://source.unsplash.com/600x800",
     address = "37th Street, Lake Orleans",
     rating = 4,
     isClosedTemporarily = true,
